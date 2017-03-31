d3964b0c0e22cc69323dc1a4a8d0e3c788d14041
rlynn1 <rlynn1@mail.csuchico.edu>
2017-03-31 16:23:01 -0700

    Added Receipt Functionality
    Added the Javascript and edited the HTML to allow for a receipt page.

--- sales.js
+++ sales.js
@@ -18,19 +18,22 @@ function addItem()
     document.getElementById("price").value="";
     setCookie(preTax, dollars, 1);
   }
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
 }
 
 //takes a number and gives a string with the number displayed as USD currency
 function asCurrency(val)
 {
   return "$" + val.toFixed(2);
+}
+
+function calculateReceipt()
+{
+    var receiptSubtotal = getCookie(preTax);
+    var receiptTax = receiptSubtotal * .075;
+    var receiptTotal = receiptSubtotal + receiptTax;
+    document.getElementById("sub").innerHTML = asCurrency(receiptSubtotal);
+    document.getElementById("tax").innerHTML = asCurrency(receiptTax);
+    document.getElementById("tot").innerHTML = asCurrency(receiptTotal);
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
