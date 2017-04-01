5857b76e71b829881249e49d2e846b55a9ef778c
rlynn1 <rlynn1@mail.csuchico.edu>
2017-03-31 17:51:26 -0700

    Attemp 11
    Just a test

--- sales.js
+++ sales.js
@@ -30,9 +30,9 @@ function calculateReceipt()
 {
     var receiptSubtotal = getCookie("preTax");
     var receiptTax = receiptSubtotal * .075;
-    var receiptTotal = Number(receiptSubtotal) + receiptTax;
+    var receiptTotal = receiptSubtotal + receiptTax;
     document.getElementById("sub").innerHTML = asCurrency(Number(receiptSubtotal));
-    document.getElementById("tax").innerHTML = asCurrency(Number(receiptTax));
+    document.getElementById("tax").innerHTML = receiptTax;
     document.getElementById("tot").innerHTML = asCurrency(Number(receiptTotal));
 }
 
